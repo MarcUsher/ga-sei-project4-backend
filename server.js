18eeec9942f2a959d6b23b1e02556af63a8d3e2a
Elisabetta <emaspero@hotmail.com>
2022-07-18 11:59:26 +0100

    added trip model and related routes/controller for adding a new trip, showing the index and showing a specific trip

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT;
 
 // Importing Routes
 const authRouter = require('./routes/auth');
+const tripRouter = require("./routes/trips");
 
 
 const bodyParser = require('body-parser')
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({
 
 // Mounting Routes
 app.use('/', authRouter);
+app.use('/', tripRouter);
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
 
