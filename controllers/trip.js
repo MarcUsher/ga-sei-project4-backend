85b8a04496456878409cc8758239fc65548f87ae
marc <marcusher@outlook.com>
2022-07-19 15:41:35 +0100

    Populated country for trip details and added new dependencies for back-end for image upload

--- controllers/trip.js
+++ controllers/trip.js
@@ -35,7 +35,7 @@ exports.trip_index_get = (req, res) => {
 exports.trip_show_get = (req, res) => {
     console.log(req.params.id);
 
-    Trip.findById(req.params.id)
+    Trip.findById(req.params.id).populate('country')
     .then(trip => {
         res.json({trip: trip})
     })
